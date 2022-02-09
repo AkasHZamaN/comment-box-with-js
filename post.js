68999cbcbf2cec276b79604c6a758e3768d83124
Akash Zaman <96652000+AkasHZamaN@users.noreply.github.com>
2022-02-09 22:21:57 +0600

    first commit

---
+++ post.js
@@ -0,0 +1,44 @@
+// add button event listener
+document.getElementById('post-btn').addEventListener('click',function(){
+    const commentBox = document.getElementById('input-filed');
+    // get user comments
+    const userComment = commentBox.value;
+    // create an html element
+    const p = document.createElement('p');
+    p.innerText = userComment;
+    // append with parent
+    const blogItem = document.getElementById('blog');
+    blogItem.appendChild(p);
+})
+
+// title css using by JS
+const h1Title = document.getElementById('title');
+h1Title.style.textAlign = 'center';
+h1Title.style.backgroundColor = 'lightblue';
+h1Title.style.padding = '10px';
+h1Title.style.borderRadius = '15px';
+
+const blogs = document.getElementById('blog');
+blogs.style.margin = '10px';
+blogs.style.padding = '5px 15px';
+blogs.style.backgroundColor = '#FCF5F5';
+blogs.style.borderRadius = '15px';
+blogs.style.color = 'black';
+
+// comment container css using by JS
+const commentContainer = document.getElementById('comment-container');
+commentContainer.style.margin = '10px';
+commentContainer.style.padding = '5px';
+// textarea css using by JS
+const inputFiled = document.getElementById('input-filed');
+inputFiled.style.border = '1px solid lightblue';
+inputFiled.style.borderRadius = '10px';
+inputFiled.style.padding = '5px';
+// btn css using by JS
+const postBtn = document.getElementById('post-btn');
+postBtn.style.backgroundColor = 'lightblue';
+postBtn.style.border = 'none';
+postBtn.style.borderRadius = '10px';
+postBtn.style.color = 'darkslateblue';
+postBtn.style.marginTop = '10px';
+postBtn.style.padding = '5px 25px';
